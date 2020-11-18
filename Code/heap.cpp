01a6efdc73a3439d29466aeead362af8ed5a5b5d
Matin Massoudi <matin@massoudi.net>
2020-11-18 16:14:58 -0700

    Implemented min-heap for vertex storage.

--- Code/heap.cpp
+++ Code/heap.cpp
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include<climits>
 #include "heap.h"
 
 using namespace std;
-using namespace heap;
+using namespace util;
 
 /**
  * Default constructor.
@@ -18,7 +19,7 @@ Heap::Heap() {}
  * @param vertexArray
  * @param heapCapacity
  */
-Heap::Heap(int *vertexArray, int heapCapacity)
+Heap::Heap(node *vertexArray, int heapCapacity)
 {
     capacity = heapCapacity;
     heapSize = 0;
@@ -27,20 +28,15 @@ Heap::Heap(int *vertexArray, int heapCapacity)
     //Populate heap with vertex elements.
     for (int i = 0; i < heapCapacity; i++)
     {
-        node newNode;
-        newNode.vertex = vertexArray[i];
-        heapArr[i] = newNode;
+        insert(vertexArray[i]);
     }
-
-    //Heapify array.
-
 }
 
 /**
  * Insert an element into the minHeap.
  * @param k
  */
-void Heap::insert(int k)
+void Heap::insert(node k)
 {
     if(heapSize == capacity)
     {
@@ -49,14 +45,70 @@ void Heap::insert(int k)
 
     heapSize = heapSize + 1;
     int i = heapSize - 1;
-    node newNode;
-    newNode.vertex = k;
-    heapArr[i] = newNode;
+    heapArr[i] = k;
 
-    while (i != 0 && heapArr[parent(i)].vertex > heapArr[i].vertex)
+    while (i != 0 && heapArr[parent(i)].distance > heapArr[i].distance)
     {
         swap(&heapArr[i], &heapArr[parent(i)]);
         i = parent(i);
     }
+}
 
+/**
+ * Extract minimum element.
+ * @return
+ */
+node Heap::extractMin()
+{
+    if(heapSize <= 0)
+    {
+        return node(-1,-1, INT_MAX);
+    }
+
+    if(heapSize == 1)
+    {
+        heapSize--;
+        return heapArr[0];
+    }
+
+    node min = heapArr[0];
+    heapArr[0] = heapArr[heapSize-1];
+    heapSize--;
+    minHeapify(0);
+    return min;
+}
+
+/**
+ * Decrease a key for a given index.
+ * @param i
+ * @param newVal
+ */
+void Heap::decreaseKey(int i, node newVal)
+{
+    heapArr[i] = newVal;
+    while(i != 0 && heapArr[parent(i)].vertex > heapArr[i].vertex)
+    {
+        swap(&heapArr[i], &heapArr[parent(i)]);
+        i = parent(i);
+    }
+}
+
+/**
+ * Minheapify algorithm that maintains min-heap properties.
+ * @param v
+ */
+void Heap::minHeapify(int v)
+{
+    int leftNode = left(v);
+    int rightNode = right(v);
+    int min = v;
+    if (leftNode < heapSize && heapArr[leftNode].distance < heapArr[v].distance)
+        min = leftNode;
+    if (rightNode < heapSize && heapArr[rightNode].distance < heapArr[min].distance)
+        min = rightNode;
+    if (min != v)
+    {
+        swap(&heapArr[v], &heapArr[v]);
+        minHeapify(v);
+    }
 }
--- Code/heap.h
+++ Code/heap.h
@@ -2,21 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "util.h"
 
 using namespace std;
+using namespace util;
 
-namespace heap {
+class Heap {
 
-    class Heap {
     private:
-
-        //min-heap struct.
-        struct node {
-            int vertex;
-            int pred;
-            int distance;
-        };
-
         //Heap array.
         node *heapArr;
         //Capacity.
@@ -33,23 +26,17 @@ namespace heap {
         int right(int index) { return 2 * index + 2; }
         //Parent subnode
         int parent(int index) { return (index - 1) / 2; }
-        //Swap nodes.
-        void swap(node *x, node *y)
-        {
-            node temp = *x;
-            *x = *y;
-            *y = temp;
-        }
+
 
     public:
         //Default constructor.
         Heap();
 
         //Overloaded constructor.
-        Heap(int *vertexArray, int capacity);
+        Heap(node *vertexArray, int capacity);
 
         //Insert into min-heap.
-        void insert(int k);
+        void insert(node k);
 
         //Return min node.
         node minimum() { return heapArr[0]; }
@@ -58,8 +45,6 @@ namespace heap {
         node extractMin();
 
         //Decrease a key value for a node.
-        void decreaseKey(int i, int newVal);
+        void decreaseKey(int i, node newVal);
 
-
-    };
-}
+};
