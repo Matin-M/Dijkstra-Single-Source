c8f7cb1f68fc767b8bb1fe05856c97e642d42c7c
Matin Massoudi <matin@massoudi.net>
2020-11-17 17:30:24 -0700

    Added files

---
+++ Code/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
