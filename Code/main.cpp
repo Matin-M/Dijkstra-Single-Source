67dfe1e54ff466b65a1abdcedea6cc54dde67ebb
Matin Massoudi <matin@massoudi.net>
2020-11-19 16:45:37 -0700

    Updated main

--- Code/main.cpp
+++ Code/main.cpp
@@ -3,12 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <climits>
+#include "graph.h"
+#include "heap.h"
 #include "util.h"
 
 using namespace std;
-using namespace util;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main()
+{
+    //Parse text file.
+    string input = "";
+    getline(cin, input);
+    //Obtain n and m;
+    int n;
+    int m;
+
     return 0;
 }
