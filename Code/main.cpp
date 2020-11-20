4c2bdaff9dbffc2973a2b5acb6a7a9238befbb0c
Matin Massoudi <matin@massoudi.net>
2020-11-19 23:48:56 -0700

    Added edge parsing

--- Code/main.cpp
+++ Code/main.cpp
@@ -5,18 +5,39 @@
 #include <climits>
 #include "graph.h"
 #include "heap.h"
+#include "main.h"
 #include "util.h"
 
 using namespace std;
 
-int main()
-{
+int Main::main() {
     //Parse text file.
     string input = "";
     getline(cin, input);
     //Obtain n and m;
-    int n;
-    int m;
+    int spaceIndex = input.find(' ');
+    int n = stoi(input.substr(0,spaceIndex));
+    int m = stoi(input.substr(spaceIndex,input.length()-1));
+
+    //Graph object that maintains graph.
+    Graph graph(n,m);
+
+    //Store edges and vertecies into graph.
+    int u;
+    int v;
+    int w;
+    for(int i = 0; i < m; i++)
+    {
+        //Parse input line and extract u, v, and w.
+        getline(cin, input);
+        spaceIndex = input.find(' ');
+        u = stoi(input.substr(0,spaceIndex));
+        v = stoi(input.substr(spaceIndex,input.length()-1));
+        spaceIndex = input.find(' ',4);
+        w = stoi(input.substr(spaceIndex,input.length()-1));
+        //Insert graphNode into graph.
+        graph.insertGraph(u,v,w);
+    }
 
     return 0;
 }
---
+++ Code/main.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <climits>
+
+using namespace std;
+
+class Main
+{
+    int main();
+};
